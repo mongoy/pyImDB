5aa70f22e13a6843c4ff6c03fc78228ed515d9e0
mongoy <mongoy@rambler.ru>
2019-11-20 15:34:15 +0900

    rework code

--- main.py
+++ main.py
@@ -2,24 +2,29 @@
 import sqlite3
 from PyQt5 import QtSql
 
-# Создаем соединение с нашей базой данных
-conn = sqlite3.connect('db.sqlite3')
-# Создаем курсор - это специальный объект который делает запросы и получает их результаты
-cursor = conn.cursor()
-# Делаем SELECT запрос к базе данных, используя обычный SQL-синтаксис
-cursor.execute("SELECT nregion FROM main_region ORDER BY nregion LIMIT 3")
-# Получаем результат сделанного запроса
-results = cursor.fetchall()
+def main():
+    # Создаем соединение с нашей базой данных
+    conn = sqlite3.connect('db.sqlite3')
+    # Создаем курсор - это специальный объект который делает запросы и получает их результаты
+    cursor = conn.cursor()
+    # Делаем SELECT запрос к базе данных, используя обычный SQL-синтаксис
+    cursor.execute("SELECT nregion FROM main_region ORDER BY nregion LIMIT 3")
+    # Получаем результат сделанного запроса
+    results = cursor.fetchall()
 
-print(results)
+    #print(results)
 
-#Не забываем закрыть соединение с базой данных
-conn.close()
+    # Не забываем закрыть соединение с базой данных
+    conn.close()
 
 
-class SQL_TEST():
 
-    pass
+
+# точка входа
+if __name__ == '__main__':
+    main()
+
+
 
 
 
