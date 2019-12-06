e5d2dded4d9f41b65232cc5c1fecad815c27cfe6
mongoy <mongoy@rambler.ru>
2019-12-06 18:21:12 +0900

    file import export2

--- im_ex.py
+++ im_ex.py
@@ -41,8 +41,9 @@ def mig_rdlist():
 
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM RDList ORDER BY naim')
+
     # SQLite RDListIO
-    # conn2 = sqlite3.connect('db.sqlite3')
+    conn2 = sqlite3.connect('db.sqlite3')
     cursor2 = conn2.cursor()
     cursor2.execute(
         ''' CREATE TABLE IF NOT EXISTS RDListIO
@@ -76,7 +77,7 @@ def mig_rdlist():
 
 
 # создаем таблици RDList и RDListIO
-mig_rdlist()
+# mig_rdlist()
 
 def mig_main_list():
     # подключение к MSSQL
@@ -90,44 +91,51 @@ def mig_main_list():
     cursor = conn.cursor()
     cursor.execute('SELECT * FROM AllGuimFull')
 
+
     # SQLite AllGuimFull
     conn2 = sqlite3.connect('db.sqlite3')
-    cursor2 = conn2.cursor()
 
     cursor2 = conn2.cursor()
     cursor2.execute(
         ''' CREATE TABLE IF NOT EXISTS AllGuimFull
             (
-             naim text, 
-             prot FLOAT,
-             invnum text,
-             idnum text,
-             bals FLOAT,
-             osts FLOAT,
-             cod text,
-             amgroup NUMERIC,
-             norma NUMERIC,
-             byear NUMERIC,
-             pikets text,
-             most1 FLOAT, am1 FLOAT, most2 FLOAT, am2 FLOAT, most3 FLOAT, am3 FLOAT, most4 FLOAT, am4 FLOAT,
-             most5 FLOAT, am5 FLOAT, most6 FLOAT, am6 FLOAT, most7 FLOAT, am7 FLOAT, most8 FLOAT, am8 FLOAT,
-             most9 FLOAT, am9 FLOAT, most10 FLOAT, am10 FLOAT, most11 FLOAT, am11 FLOAT, most12 FLOAT, am12 FLOAT,
-             most13 FLOAT, am13 FLOAT
+            nnp NUMERIC,
+            obj NUMERIC,
+            cobj NUMERIC,
+            mobj text, 
+            naim text,
+            mat text, 
+            lp NUMERIC,
+            addr text,
+            prot FLOAT,     
+            invnum text,
+            idnum text
             )
         '''
     )
 
+# база Sqlite
     rows = cursor.fetchall()
-    str_q = 'INSERT INTO RDListIO VALUES(' + ','.join(['?' for _ in range(37)]) + ')'
-    cursor2.executemany(str_q, ([[(row[i]) for i in range(37)] for row in rows]))
+    str_q = 'INSERT INTO AllGuimFull VALUES(' + ','.join(['?' for _ in range(11)]) + ')'
+    print(str_q)
+    #print([[(row[i]) for i in range(65)] for row in rows])
+    # f = open('text.txt', 'w')
+    # for row in rows:
+    #     for i in range(65):
+    #         f.write(str(row[i]) + ', ')
+    #     f.write('\n')
+    # f.close()
+    cursor.executemany(str_q, ([[(row[i]) for i in range(11)] for row in rows]))
 
     conn2.commit()
     conn2.close()
-    return
+
     return
 
+mig_main_list()
 
-def up_main_tb():
+
+def up_rd_list_io():
     # подключение к MSSQL
     server = 'BUDA\\SQLEXPRESS'
     database = 'LISTIM2015'
@@ -137,5 +145,18 @@ def up_main_tb():
               ';PWD=' + password
     conn = pyodbc.connect(con_str)
     cursor = conn.cursor()
-    cursor.execute('SELECT * FROM RDList')
+    str_q = "SELECT RDList.naim FROM RDList \
+    , sum(AllGuimFull.bals) AS sum_bal \
+    , SUM(AllGuimFull.balost) AS sum_ost \
+    INNER JOIN AllGuimFull \
+    ON RDList.naim = AllGuimFull.mobj \
+    GROUP BY RDList.naim ORDER BY RDList.naim"
+    print(str_q)
+    cursor.execute(str_q)
+    rows1 = cursor.fetchall()
+
     return
+
+
+#
+#up_rd_list_io()
