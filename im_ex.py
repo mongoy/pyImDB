5d59ba1c090be24311c3d45a584c4592dc00dcfa
mongoy <mongoy@rambler.ru>
2019-12-09 14:41:35 +0900

    file import export4

--- im_ex.py
+++ im_ex.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pyodbc
+import csv
 
 # подключение к MSSQL
 server = 'BUDA\\SQLEXPRESS'
@@ -18,78 +19,89 @@ def mig_main_list():
     try:
         # MSSQL
         cursor = conn.cursor()
-        cursor.execute('SELECT * FROM AllGuimFull')
-
+        cursor.execute('SELECT * FROM AllGuimFull_C')
 
         # SQLite
         cursor2 = conn2.cursor()
-        cursor2.execute(
-            ''' CREATE TABLE IF NOT EXISTS AllGuimFull
-                    (
-                    nnp INTEGER,
-                    obj INTEGER,
-                    cobj INTEGER,
-                    mobj TEXT, 
-                    naim TEXT,
-                    mat TEXT, 
-                    lp NUMERIC,
-                    addr TEXT,
-                    prot REAL,     
-                    invnum TEXT,
-                    idnum TEXT,
-                    bals REAL,
-                    balost REAL,
-                    okof TEXT,
-                    norma NUMERIC,
-                    gramort NUMERIC,
-                    byear TEXT,
-                    prim TEXT,
-                    primper TEXT,
-                    ops TEXT,
-                    pasportbti TEXT,
-                    zemnum TEXT,
-                    numgosreg TEXT,
-                    disl TEXT,
-                    piket REAL,
-                    piketb REAL,
-                    pikete REAL,
-                    pmobj NUMERIC,
-                    pernum NUMERIC,
-                    onbal INTEGER,
-                    prewriteoff INTEGER,
-                    assignment INTEGER,
-                    datain TEXT,
-                    docin TEXT,
-                    dataout TEXT,
-                    docout TEXT,
-                    inform TEXT,
-                    most1 REAL, am1 REAL, most2 REAL, am2 REAL, 
-                    most3 REAL, am3 REAL, most4 REAL, am4 REAL,
-                    most5 REAL, am5 REAL, most6 REAL, am6 REAL, 
-                    most7 REAL, am7 REAL, most8 REAL, am8 REAL,
-                    most9 REAL, am9 REAL, most10 REAL, am10 REAL, 
-                    most11 REAL, am11 REAL, most12 REAL, am12 REAL,
-                    most13 REAL, am13 REAL,
-                    ybase TEXT,
-                    cat2 TEXT,
-                    cat3 TEXT,
-                    cat4 TEXT,
-                    cat5 TEXT,
-                    cat6 TEXT 
-                    )
-                '''
-        )
-
+        # cursor2.execute(
+        #     ''' CREATE TABLE IF NOT EXISTS AllGuimFull
+        #             (
+        #             nnp INTEGER,
+        #             obj INTEGER,
+        #             cobj INTEGER,
+        #             mobj TEXT,
+        #             naim TEXT,
+        #             mat TEXT,
+        #             lp NUMERIC,
+        #             addr TEXT,
+        #             prot REAL,
+        #             invnum TEXT,
+        #             idnum TEXT,
+        #             bals REAL,
+        #             balost REAL,
+        #             okof TEXT,
+        #             norma NUMERIC,
+        #             gramort NUMERIC,
+        #             byear TEXT,
+        #             prim TEXT,
+        #             primper TEXT,
+        #             ops TEXT,
+        #             pasportbti TEXT,
+        #             zemnum TEXT,
+        #             numgosreg TEXT,
+        #             disl TEXT,
+        #             piket REAL,
+        #             piketb REAL,
+        #             pikete REAL,
+        #             pmobj NUMERIC,
+        #             pernum NUMERIC,
+        #             onbal INTEGER,
+        #             prewriteoff INTEGER,
+        #             assignment INTEGER,
+        #             datain TEXT,
+        #             docin TEXT,
+        #             dataout TEXT,
+        #             docout TEXT,
+        #             inform TEXT,
+        #             most1 REAL, am1 REAL, most2 REAL, am2 REAL,
+        #             most3 REAL, am3 REAL, most4 REAL, am4 REAL,
+        #             most5 REAL, am5 REAL, most6 REAL, am6 REAL,
+        #             most7 REAL, am7 REAL, most8 REAL, am8 REAL,
+        #             most9 REAL, am9 REAL, most10 REAL, am10 REAL,
+        #             most11 REAL, am11 REAL, most12 REAL, am12 REAL,
+        #             most13 REAL, am13 REAL,
+        #             ybase TEXT,
+        #             cat2 TEXT,
+        #             cat3 TEXT,
+        #             cat4 TEXT,
+        #             cat5 TEXT,
+        #             cat6 TEXT
+        #             )
+        #         '''
+        # )
+        # reader = csv.reader(open('expdata1.csv', 'r'), delimiter=';')
+        # print(reader)
+        # for row in reader:
+        # to_db = [unicode(row[0], "utf8"), unicode(row[1], "utf8"), unicode(row[2], "utf8")]
+        # curs.execute("INSERT INTO PCFC (type, term, definition) VALUES (?, ?, ?);", to_db)
         rows = cursor.fetchall()
         str_q = 'INSERT INTO AllGuimFull VALUES(' + ','.join(['?' for _ in range(69)]) + ')'
         cursor2.executemany(str_q, ([[(row[i]) for i in range(69)] for row in rows]))
+        # str_q = 'INSERT INTO AllGuimFull VALUES(' + ','.join(['?' for _ in range(10)]) + ')'
+        # cursor2.executemany(str_q, ([[(rw[i]) for i in range(10)] for rw in row]))
 
     except sqlite3.DatabaseError as err:
         print("Error: ", err)
     else:
         conn2.commit()
+        conn2.close()
+    return
+
+
+# mig_main_list()
+
+def mig_rd_list():
 
     return
 
-mig_main_list()
-conn2.close()
+# mig_rd_list()
